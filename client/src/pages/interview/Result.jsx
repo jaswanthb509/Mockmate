6c80e61c05e661f0798d97b53856bc0e9d7e94cf
jaswanthb509 <jashuruthvik123@gmail.com>
2026-07-30 20:19:13 +0530

    React router setup and frontend page structure is been done

---
+++ client/src/pages/interview/Result.jsx
@@ -0,0 +1,5 @@
+const Result = () => {
+  return <h1>Interview Result</h1>;
+};
+
+export default Result;
---
+++ client/src/pages/interview/SetupInterview.jsx
@@ -0,0 +1,5 @@
+const SetupInterview = () => {
+  return <h1>Setup Interview</h1>;
+};
+
+export default SetupInterview;
